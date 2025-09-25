fc1ab0c3e2259e51e7c52bef3b825d603f3acc62
injailoutsoon <46853989+injailoutsoon@users.noreply.github.com>
2025-09-24 22:12:22 -0400

    fix(auth): fix AuthGatewayApplicationTests to make build succeed

--- auth/src/test/java/ca/etsmtl/taf/auth/AuthGatewayApplicationTests.java
+++ auth/src/test/java/ca/etsmtl/taf/auth/AuthGatewayApplicationTests.java
@@ -1,13 +1,29 @@
 package ca.etsmtl.taf.auth;
 
+import ca.etsmtl.taf.auth.repository.RoleRepository;
+import ca.etsmtl.taf.auth.repository.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
+@EnableAutoConfiguration(exclude = {
+        org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration.class,
+        org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration.class
+})
 class AuthGatewayApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @MockBean
+    private RoleRepository roleRepository;
+
+    @MockBean
+    private UserRepository userRepository;
+
+    @Test
+    void contextLoads() {
+    }
 
 }
